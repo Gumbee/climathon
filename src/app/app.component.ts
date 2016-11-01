aca03ef810960dcb1a4258c080b86a3cbf9b3c21
gumbee <mugeeb.hassan@gmail.com>
2016-11-01 18:32:11 +0100

    initialized firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 import { ProfilePage } from '../pages/profile/profile';
 import { MarketPage } from '../pages/market/market';
-
+import { firebaseConfig } from '../../config/config';
+import firebase from 'firebase';
 
 @Component({
   templateUrl: '../app/app.html'
@@ -13,6 +14,9 @@ export class MyApp {
   rootPage = ProfilePage;
 
   constructor(platform: Platform, public menuCtrl: MenuController) {
+    
+    firebase.initializeApp(firebaseConfig);
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
